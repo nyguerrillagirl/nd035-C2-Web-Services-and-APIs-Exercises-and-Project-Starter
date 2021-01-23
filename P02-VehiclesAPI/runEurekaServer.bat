3e278489d2966bf2e5ebe791e8e0c12869a5ac88
lafig <lafig@DESKTOP-G57UGNQ.myfiosgateway.com>
2021-01-23 18:06:58 -0500

    Adding new project for EurekaServer

---
+++ P02-VehiclesAPI/runEurekaServer.bat
@@ -0,0 +1,2 @@
+# First start Eureka Server
+java -jar ./EurekaServer/target/EurekaServer-0.0.1-SNAPSHOT.jar
---
+++ P02-VehiclesAPI/runbooglemaps.bat
@@ -0,0 +1,4 @@
+# First start Eureka Server
+
+# Start boogle-Maps
+java -jar ./boogle-maps/target/boogle-maps-0.0.1-SNAPSHOT.jar
